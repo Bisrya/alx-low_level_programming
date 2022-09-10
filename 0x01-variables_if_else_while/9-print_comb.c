afd3037af7f24de37b92b664f195753da1ddc336
Bisry <bisryk2021@gmail.com>
2022-09-10 04:19:22 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - This program prints the numbers 0 to 9 separated
+ * by a comma and a space, minus the number 9.
+ *
+ * Return: 0.
+ */
+
+int main(void)
+{
+int d;
+
+for (d = '0'; d <= '9'; d++)
+{
+putchar(d);
+
+if (d != '9')
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+
+return (0);
+}
